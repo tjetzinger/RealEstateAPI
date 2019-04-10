b63dcb6d2bde97cfc9d7d968e951dbf5636411bd
Thomas Jetzinger <thomas@jetzinger.com>
2019-04-10 21:37:45 +0200

    Respond with Errors

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -1,12 +1,10 @@
 const config = require('config');
 const { APIError, InternalServerError } = require('rest-api-errors');
 const { STATUS_CODES } = require('http');
-//const logger = require('../../logger');
 
 // eslint-disable-next-line
 const errorHandler = (err, req, res, next) => {
-    const error = (err.status === 401 ||
-        err instanceof APIError) ? err : new InternalServerError();
+    const error = (err.status === 401 || err instanceof APIError) ? err : new InternalServerError();
 
     if (process.env.NODE_ENV !== 'production') {
         // eslint-disable-next-line
@@ -16,21 +14,19 @@ const errorHandler = (err, req, res, next) => {
         console.log(err);
     }
 
-
     // if (err.name === 'ValidationError') {
     //   return res.status(405).json(err);
     // }
 
-    //logger.info('API error', { error: err });
+    console.log('API error', { error: err });
 
-    // return res
-    //   .status(error.status || 500)
-    //   .json({
-    //     code: error.code || 500,
-    //     message: error.message || STATUS_CODES[error.status],
-    //   });
+    return res
+      .status(error.status || 500)
+      .json({
+        code: error.code || 500,
+        message: error.message || STATUS_CODES[error.status],
+      });
 
-    res.json(config.messages.errorValuation);
 };
 
 module.exports = { errorHandler };
