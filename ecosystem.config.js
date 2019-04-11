79d763a319182449e994307ef3a1944e43687ead
Thomas Jetzinger <thomas@jetzinger.com>
2019-04-11 09:31:20 +0200

    Added Process File

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+const env = process.argv[process.argv.indexOf('--env') + 1];
+const isProd = env === 'prod';
+
+module.exports = {
+    apps : [{
+        name: isProd ? 'RealEstateAPI-Prod' : 'RealEstateAPI-Int',
+        script: 'npm',
+        args: '-- start',
+        instances: isProd ? 'max' : 1,
+        exec_mode : isProd ? 'cluster' : 'fork',
+        watch: true,
+        ignore_watch : ["node_modules", "logs"],
+        error_file: 'logs/error.log',
+        out_file: 'logs/output.log',
+        env_int: {
+            "NODE_ENV": 'int'
+        },
+        env_prod : {
+            "NODE_ENV": 'prod'
+        }
+    }]
+};
