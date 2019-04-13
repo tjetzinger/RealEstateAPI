a84eefc9eec28db2ae5326e8a3d4accd946cbe12
Thomas Jetzinger <thomas@jetzinger.com>
2019-04-13 14:31:30 +0200

    More Responsive Error Logging

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -2,23 +2,14 @@ const config = require('config');
 const { APIError, InternalServerError } = require('rest-api-errors');
 const { STATUS_CODES } = require('http');
 
-// eslint-disable-next-line
 const errorHandler = (err, req, res, next) => {
     const error = (err.status === 401 || err instanceof APIError) ? err : new InternalServerError();
 
-    if (process.env.NODE_ENV !== 'production') {
-        // eslint-disable-next-line
-        console.log('-----> Unknown server error...');
-        // todo: comment here for production
-        // eslint-disable-next-line
-        console.log(err);
+    console.log(error);
+
+    if (err.name === 'ValidationError') {
+      return res.status(405).json(err);
     }
-
-    // if (err.name === 'ValidationError') {
-    //   return res.status(405).json(err);
-    // }
-
-    console.log('API error', { error: err });
 
     return res
       .status(error.status || 500)
